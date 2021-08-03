5758504fadae0cb22fd49bb4f918264400613f0a
jiwon <cook1804@gmail.com>
2021-08-04 02:05:11 +0900

    setting 클래스 만들고 alien_invasion 수정

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,15 +7,20 @@
 import sys # 플레이어가 게임을 끝낼 때 필요한 도구가 있다.
 import pygame  # 게임을 만드는 데 필요한 기능이 들어있다.
 
+from settings import Settings
+
 class AlienInvasion:
     """게임 전체의 자원과 동작을 관리하는 클래스"""
 
     def __init__(self):
         pygame.init() # 파이게임이 정상적으로 동작하기 위해 필요한 세팅을 초기화 한다. 
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800)) # pygame.display.set_mode()를 호출해 디스플레이 창을 생성. 매개변수 (1200,800)은 게임 창 크기를 정하는 튜플
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height)) # pygame.display.set_mode()를 호출해 디스플레이 창을 생성. 매개변수 (1200,800)은 게임 창 크기를 정하는 튜플
         pygame.display.set_caption("Alien Invasion")
-    
+        
+        # 배경색을 설정합니다.
+        self.bg_color = (230, 230, 230)
     def run_game(self):
         """게임의 메인 루프를 시작합니다."""
         while True:
@@ -23,6 +28,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            #루프의 반복마다 화면을 다시 그립니다.
+            self.screen.fill(self.settings.bg_color)
             
             #가장 최근에 그려진 화면을 표시합니다. 
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """외계인 침공 게임의 세팅을 모두 저장하는 클래스"""
+
+    def __init__(self):
+        """게임 세팅을 초기화합니다."""
+        # 화면 세팅
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
